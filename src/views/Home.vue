ce47fc3aa85fb701f9e1c0ea376e4211664e697c
Ken Baum <ilpadre1953@gmail.com>
2020-01-27 14:59:55 -0500

    Changed text

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld msg="Ken's Excellent Book List"/>
   </div>
 </template>
 
