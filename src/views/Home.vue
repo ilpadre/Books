0c43d4424e989d7bcac8ebbb0ab2fafc0ca8e109
Ken Baum <ilpadre1953@gmail.com>
2020-04-25 21:39:21 -0400

    Decanadianize the text

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Ken's Excellent Book List, Eh?"/>
+    <HelloWorld msg="Ken's Excellent Book List"/>
   </div>
 </template>
 
